54cc58190bea73271c87e5cbcd0bc92ba765d3e5
matthias springstein <matthias.springstein@tib.eu>
2018-08-07 16:28:40 +0200

    Update template style.

--- build.sh
+++ build.sh
@@ -1,23 +1,10 @@
 #! /bin/bash
-
-#pdflatex -output-directory=build -halt-on-error bachelor.tex;
-
-#makeindex build/bachelor.nlo -s nomencl.ist -o build/bachelor.nls;
-# makeindex build/master.nlo -s nomencl.ist -o build/master.nls;
-# pdflatex -output-directory=build -halt-on-error master;
-# cp *.bib build/
-# cd build
-# bibtex master
-# cd ..
-# pdflatex -output-directory=build -halt-on-error master;
-# pdflatex -output-directory=build -halt-on-error master;
-
-
-pdflatex -output-directory=build -halt-on-error presentation.tex;
-pdflatex -output-directory=build -halt-on-error presentation.tex;
-
-# pdflatex -output-directory=build -halt-on-error presentation_defense.tex;
-# pdflatex -output-directory=build -halt-on-error presentation_defense.tex;
-
-# pdflatex -output-directory=build -halt-on-error test.tex;
-# pdflatex -output-directory=build -halt-on-error test.tex;
+mkdir -p build
+pdflatex -output-directory=build -halt-on-error presentation;
+cp *.bib build/
+cp *.bst build/
+cd build
+bibtex presentation
+cd ..
+pdflatex -output-directory=build -halt-on-error presentation;
+pdflatex -output-directory=build -halt-on-error presentation;
